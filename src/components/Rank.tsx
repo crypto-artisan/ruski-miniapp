095afedaea965a5af69d29d48cc650e9216d1a82
hightecular95908 <rkulkin47@gmail.com>
2024-07-04 14:29:23 +0900

    Refferal System Completed

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState, useRef } from "react";
-import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import Loader from "./Loader";
 import { ENDPOINT } from "../data";
 
@@ -41,10 +41,10 @@ const Rank: React.FC<IRankProps> = ({ user }) => {
     return (
         <div className="h-screen flex flex-col w-full justify-between items-center gap-4 pb-[90px]">
             <div className="w-full">
-                <div className="flex px-3 text-lg w-full text-[#2ea6d9f0]">
-                    <div className="text-start w-[20%]">Rank</div>
-                    <div className="text-start w-[55%]">User</div>
-                    <div className="text-start w-[20%]">$Ruski</div>
+                <div className="flex px-3 text-lg w-full text-[#2ea6d9f0] items-center justify-between">
+                    <div className="text-center w-[20%]">Rank</div>
+                    <div className="text-center w-[60%]">User</div>
+                    <div className="text-center w-[30%]">$Ruski</div>
                 </div>
                 {
                     loading ? (
@@ -58,17 +58,17 @@ const Rank: React.FC<IRankProps> = ({ user }) => {
                                     key={index}
                                     className={`flex px-3 py-1 items-center bg-[#2ea6d9f0] w-full`}
                                 >
-                                    <div className="text-lg max-sm:text-lg text-start pl-2 w-[20%]">{index + 1}</div>
+                                    <div className="text-lg text-center pl-2 w-[20%]">{index + 1}</div>
                                     <div className="relative h-10 overflow-hidden w-[60%] flex items-center">
                                         <img
                                             src="/logo.png"
                                             alt="avatar"
                                             className="w-12 h-12 rounded-full"
                                         />
-                                        <p className="text-lg max-sm:text-lg text-start pl-2">{iUser.userName}</p>
+                                        <p className="text-medium text-start pl-2">{iUser.userName}</p>
                                     </div>
 
-                                    <p className="text-lg max-sm:text-lg text-start pl-2 w-[30%]">
+                                    <p className="text-medium pl-2 w-[30%] text-center">
                                         {formatNumberWithCommas(iUser.totalPoints)}
                                     </p>
                                 </div>
@@ -81,12 +81,12 @@ const Rank: React.FC<IRankProps> = ({ user }) => {
             <div
                 className={`flex px-3 py-2 items-center bg-[#227ea6] rounded-lg w-full`}
             >
-                <div className="text-lg max-sm:text-lg text-start pl-2 w-[20%]">{ranking + 1}</div>
+                <div className="text-lg text-center pl-2 w-[20%]">{ranking + 1}</div>
                 <div className="relative h-12 overflow-hidden w-[60%] flex items-center">
                     <img src="/logo.png" alt="avatar" className="w-12 h-12 rounded-full" />
-                    <p className="text-lg max-sm:text-lg text-start pl-2">{curUser.userName}</p>
+                    <p className="text-lg text-start pl-2">{curUser.userName}</p>
                 </div>
-                <p className="text-lg max-sm:text-lg text-start pl-2 w-[30%]">
+                <p className="text-lg text-center pl-2 w-[30%]">
                     {formatNumberWithCommas(curUser.totalPoints)}
                 </p>
             </div>
